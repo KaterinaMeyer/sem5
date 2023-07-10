ea0859727abb0d45a782e3482b39f76c5688af7d
Катерина <katttum@ya.ru>
2023-07-10 20:48:36 +0300

    массив из положительных чисел сумма четных + функция

---
+++ task1/Program.cs
@@ -0,0 +1,45 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите размер массива: ");
+        int size = int.Parse(Console.ReadLine());
+
+        int[] array = new int[size];
+
+        Random random = new Random();
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(100, 1000);
+        }
+
+        Console.WriteLine("Сгенерированный массив:");
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+
+        int evenCount = CountEvenNumbers(array);
+
+        Console.WriteLine("Количество чётных чисел в массиве: " + evenCount);
+
+        Console.ReadLine();
+    }
+
+    static int CountEvenNumbers(int[] arr)
+    {
+        int count = 0;
+        foreach (int num in arr)
+        {
+            if (num % 2 == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
